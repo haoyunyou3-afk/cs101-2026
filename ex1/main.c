23cfdcd6bb6382b80909d64deb1739a0f0d10608
haoyunyou3-afk <haoyunyou3@gmail.com>
2026-09-24 13:38:31 +0800

    Create main.c

---
+++ ex1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\t\t2025 九月\n");
+    printf("日\t一\t二\t三\t四\t五\t六\n");
+    printf("\033[1;34m31") ;
+    printf("\033[1;36m\t 1\t 2\t 3\t 4\t 5\t 6\n");
+    printf(" 7\t 8\t 9\t 10\t 11\t 12\t 13\n");
+    printf(" 14\t 15\t 16\t 17\t 18\t 19\t 20\n");
+    printf(" 21\t 22\t 23\t 24\t 25\t 26\t 27\n");
+    printf(" 28\t 29\t 30\t ");
+    printf("\033[1;34m 1\t 2\t 3\t 4\n");
+    return 0;
+}
